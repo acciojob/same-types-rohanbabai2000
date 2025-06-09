f49f5cc2ee0b3350ec71fdd8524ad4984760a17d
ROHAN DAS <61402717+rohanbabai2000@users.noreply.github.com>
2025-06-09 08:31:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ function isSameType(value1, value2) {
   //your js code here
 	if (typeof(value1)==typeof(value2)){
 		return true;
+	}else{
+		return false;
 	}
-	return false;
+	
 }
 
 // do not change the code below.
